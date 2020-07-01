427a23f2a9e835d4cc6b07056943b2f0084855af
brad <zero@accessdenied.org>
2020-07-01 18:19:17 -0400

    more progress on an actual UI! slow & steady

--- app/routes.py
+++ app/routes.py
@@ -1,28 +1,18 @@
 from app import app
 from app import roll
-from flask_wtf import FlaskForm
-from wtforms import StringField, TextField, SubmitField
-from wtforms.validators import DataRequired, Length
-import os
-SECRET_KEY = os.urandom(32)
-app.config['SECRET_KEY'] = SECRET_KEY
-
-
-class DiceForm(FlaskForm):
-    die = StringField('dice to roll', [DataRequired()])
-    mod = StringField('modifier')
-    submit = SubmitField('Submit')
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    form = DiceForm()
-    if form.validate_on_submit():
-        print("yup")
-
-
-@app.route('/dice')
-def dice():
-    # we are passing hard coded values for the dice and modifier, for now
-    return str(roll.roll_em("6d20", "6"))
+    user = {'username': 'brad'}
+    return '''
+<html>
+    <head>
+        <title>Dice roll!</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+        <h1> ''' + str(roll.roll_em("6d20", "12")) + ''' </h1>
+    </body>
+</html>'''
