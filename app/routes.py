21b32bb35906e7eac66f9e49f5adc85f0805b5c9
brad <zero@accessdenied.org>
2020-07-01 22:56:21 -0400

    tidyness

--- app/routes.py
+++ app/routes.py
@@ -2,11 +2,9 @@ from app import app
 from flask import render_template
 from app.forms import RollDice
 
+
 @app.route('/')
 @app.route('/index')
 def index():
-    # user = {'username': 'brad'}
-    # return render_template('index.html', title='DICE', user=user)
     form = RollDice()
     return render_template('roll.html', title='ROLL', form=form)
-
